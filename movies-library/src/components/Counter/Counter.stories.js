34162f3b68fd6bd6ddd6b6ea40179cf0ca40a6ed
Paulina_Kwapisz <paulina_kwapisz@epam.com>
2023-09-26 11:19:21 +0200

    storybook fix

--- movies-library/src/components/Counter/Counter.stories.js
+++ movies-library/src/components/Counter/Counter.stories.js
@@ -1,16 +1,11 @@
-/* eslint-disable import/no-anonymous-default-export */
 import React from "react";
+
 import Counter from "./Counter";
 
 export default {
   title: "Example/Counter",
+
   component: Counter,
 };
 
 export const BasicCounter = () => <Counter initialCount={1} />;
-
-export const Secondary = {
-  args: {
-    initialCount: "1",
-  },
-};
